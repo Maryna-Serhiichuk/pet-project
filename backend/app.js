fceb82882dc37139d22707ec5bd0d1e86e11bce3
Maryna-Serhiichuk <marina.serhiichuk@gmail.com>
2021-09-30 19:16:52 +0300

    from nodejs to express

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,72 @@ const configDB = require('./config/db.js');
 
 const conn = mysql.createConnection(configDB);
 
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({extended:true});
+
 const dbNameTable = 'goods';
 
+app.listen(3001);
 
+app.route('/goods')
+    .get((req, res) => {
+        getGoods(req, res);
+    })
+    .post(urlencodedParser, (req, res) => {
+        addNewGoods(req, res);
+    })
+    .put(urlencodedParser, (req, res) => {
+        updateGoods(req, res);
+    })
+    .delete((req, res) => {
+        deleteGoods(req, res);
+    })
+
+function getGoods(req, res){
+    conn.query(`SELECT * FROM ${dbNameTable}`, (err, result, field) => {
+        let content = JSON.stringify(result);
+        res.send(content);
+    })
+}
+
+function addNewGoods(req, res){
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    })
+    req.on('end', () => {
+        let result = JSON.parse(body);
+        conn.query(`INSERT INTO ${dbNameTable} (title, price, info, image) VALUES ('${result.title}', '${result.price}', '${result.info}', '${result.image}');`, (err, result, field) => {
+            console.log(err)
+            res.send();
+        })
+    })
+}
+
+function updateGoods(req, res){
+    let body = '';
+    req.on('data', chunk => {
+        body += chunk.toString();
+    })
+    req.on('end', () => {
+        let result = JSON.parse(body);
+
+        conn.query(`UPDATE ${dbNameTable} SET title = '${result.title}', price = ${result.price}, info = '${result.info}', image = '${result.image}' WHERE id = ${result.id}`, (err, result, field) => {
+            res.send();
+        })
+    })
+}
+
+function deleteGoods(req, res){
+    let urlInfo = url.parse(req.url, true);
+
+    conn.query(`DELETE FROM ${dbNameTable} WHERE id = ${urlInfo.query.delete}`, (err, result, field) => {
+        res.send();
+    })
+}
 
 // CREATE TABLE goods ( // створення таблички
 // 	id INT NOT NULL AUTO_INCREMENT,
@@ -24,99 +87,3 @@ const dbNameTable = 'goods';
 // ('London City','5','London eat'), 
 // ('Hachapuri','1','Station eat'), 
 // ('Gamburger','12','McDonalds ate');
-
-http.createServer( (req, res) => {
-    let urlInfo = url.parse(req.url, true);
-    // console.log(req)
-
-    if(req.method == 'GET'){
-        switch(urlInfo.pathname){
-            case '/goods':
-                getGoods(req, res);
-                break;
-            default:
-                error404(req, res);
-                break;
-        }
-    }
-    else if (req.method == 'POST'){
-        switch(urlInfo.pathname){
-            case '/goods':
-                addNewGoods(req, res)
-                break;
-            default:
-                error404(req, res);
-                break;
-        }
-    }
-    else if (req.method == 'PUT'){
-        switch(urlInfo.pathname){
-            case '/goods':
-                updateGoods(req, res);
-                break;
-            default:
-                error404(req, res);
-                break;
-        }
-    }
-    else if (req.method == 'DELETE'){
-        switch(urlInfo.pathname){
-            case '/goods':
-                deleteGoods(req, res)
-                break;
-            default:
-                error404(req, res);
-                break;
-        }
-    } else {
-        error404(req, res)
-    }
-}).listen(3001, () => {
-    console.log('work')
-})
-
-function error404(req, res){
-    res.end('404 not found');
-}
-
-function getGoods(req, res){
-    conn.query(`SELECT * FROM ${dbNameTable}`, (err, result, field) => {
-        let content = JSON.stringify(result);
-        res.end(content);
-    })
-}
-function addNewGoods(req, res){
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    })
-    req.on('end', () => {
-        let result = JSON.parse(body);
-        conn.query(`INSERT INTO ${dbNameTable} (title, price, info, image) VALUES ('${result.title}', '${result.price}', '${result.info}', '${result.image}');`, (err, result, field) => {
-            console.log(err)
-            res.end();
-        })
-    })
-}
-
-function updateGoods(req, res){
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk.toString();
-    })
-    req.on('end', () => {
-        let result = JSON.parse(body);
-
-        conn.query(`UPDATE ${dbNameTable} SET title = '${result.title}', price = ${result.price}, info = '${result.info}', image = '${result.image}' WHERE id = ${result.id}`, (err, result, field) => {
-            res.end();
-        })
-    })
-}
-
-function deleteGoods(req, res){
-    let urlInfo = url.parse(req.url, true);
-
-    conn.query(`DELETE FROM ${dbNameTable} WHERE id = ${urlInfo.query.delete}`, (err, result, field) => {
-        res.end();
-    })
-}
